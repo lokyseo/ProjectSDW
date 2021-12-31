e197a8d566f9a775c77ea1ff216fe715762e4f6f
00GOEUN <88764141+ChoDanBi@users.noreply.github.com>
2021-12-31 14:47:38 +0900

    subway

---
+++ subway/Assets/Scripts/MoveObjects.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveObjects : MonoBehaviour {
+
+	public float moveSpeed;
+	public float addSpeed;
+	public float speedTimer;
+	public float scoreTimer;
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+		if (!PlayerMove.dead) {
+			speedTimer += Time.deltaTime;
+			scoreTimer += Time.deltaTime;
+			if (scoreTimer >= 2) {
+				scoreTimer = 0;
+                PlayerMove.score += 10;
+			}
+			if (speedTimer >= 10) {
+				speedTimer = 0;
+				moveSpeed += addSpeed;
+				Debug.Log ("increased");
+			}
+			transform.Translate (0, 0, -moveSpeed * Time.deltaTime);
+		}
+	}
+}
---
+++ subway/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public static PlayerMove Instance()
+    {
+        return instance;
+    }
+    private static PlayerMove instance = null;
+
+    private void Awake()
+    {
+        if (instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+    }
+
+    public static bool dead;
+    public static int score;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+           
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+
+        }
+    }
+}

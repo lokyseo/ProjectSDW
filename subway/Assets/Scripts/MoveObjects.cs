f3a13b8d9d72a627c8b11505bc63932637c59d1f
00GOEUN <88764141+ChoDanBi@users.noreply.github.com>
2022-01-04 14:51:30 +0900

    01.04(subway)

--- subway/Assets/Scripts/MoveObjects.cs
+++ subway/Assets/Scripts/MoveObjects.cs
@@ -15,19 +15,22 @@ public class MoveObjects : MonoBehaviour {
 	
 	void Update ()
     {
-		if (!PlayerMove.dead) {
-			speedTimer += Time.deltaTime;
-			scoreTimer += Time.deltaTime;
-			if (scoreTimer >= 2) {
-				scoreTimer = 0;
-                PlayerMove.score += 10;
-			}
-			if (speedTimer >= 10) {
-				speedTimer = 0;
-				moveSpeed += addSpeed;
-				Debug.Log ("increased");
-			}
-			transform.Translate (0, 0, -moveSpeed * Time.deltaTime);
+		if (!PlayerMove.dead)
+        {
+			//speedTimer += Time.deltaTime;
+			//scoreTimer += Time.deltaTime;
+			//if (scoreTimer >= 2)
+            //{
+			//	scoreTimer = 0;
+            //    PlayerMove.score += 10;
+			//}
+			//if (speedTimer >= 10)
+            //{
+			//	speedTimer = 0;
+			//	moveSpeed += addSpeed;
+			//	Debug.Log("increased");
+			//}
+			transform.Translate(0, 0, -moveSpeed * Time.deltaTime);
 		}
 	}
 }
